cc60c0cda5e36abbdd9e8b009559f75ea76c4a4d
Richard Hunter <richard@richardhunter.co.uk>
2020-10-14 00:38:05 +0100

    updating cdn link

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ requirejs.config({
     'backbone' : 'https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.3.3/backbone-min',
     'underscore' : 'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min',
     'jquery' : 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min',
-    'injector' : 'https://unpkg.com/Diogenes@1.4.9/lib/injector'
+    'injector' : 'https://unpkg.com/Diogenes@1.4.14/lib/injector'
   }
 });
 
